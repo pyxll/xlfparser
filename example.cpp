1ad36b9b3474a8441f9b2b114058822599ca519d
Tony Roberts <tony@pyxll.com>
2019-02-25 16:01:29 +0000

    Initial version of xlfparser

---
+++ example.cpp
@@ -0,0 +1,156 @@
+/*
+The MIT License
+
+Copyright (c) 2019 PyXLL Ltd. https://www.pyxll.com
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+*/
+#include "xlfparser.h"
+#include <string>
+#include <vector>
+#include <iostream>
+
+using namespace xlfparser;
+
+std::ostream& operator<<(std::ostream& os, Token::Type t)
+{
+    switch (t)
+    {
+        case Token::Type::Operand:
+            os << "Operand";
+            break;
+        case Token::Type::Function:
+            os << "Function";
+            break;
+        case Token::Type::Array:
+            os << "Array";
+            break;
+        case Token::Type:: ArrayRow:
+            os << "ArrayRow";
+            break;
+        case Token::Type::Subexpression:
+            os << "Subexpression";
+            break;
+        case Token::Type::Argument:
+            os << "Argument";
+            break;
+        case Token::Type::OperatorPrefix:
+            os << "OperatorPrefix";
+            break;
+        case Token::Type::OperatorInfix:
+            os << "OperatorInfix";
+            break;
+        case Token::Type::OperatorPostfix:
+            os << "OperatorPostfix";
+            break;
+        case Token::Type::Whitespace:
+            os << "Whitespace";
+            break;
+        case Token::Type::Unknown:
+        default:
+            os << "Unknown";
+            break;
+    }
+}
+
+std::ostream& operator<<(std::ostream& os, Token::Subtype t)
+{
+    switch (t)
+    {
+        case Token::Subtype::None:
+            os << "None";
+            break;
+        case Token::Subtype::Start:
+            os << "Start";
+            break;
+        case Token::Subtype::Stop:
+            os << "Stop";
+            break;
+        case Token::Subtype::Text:
+            os << "Text";
+            break;
+        case Token::Subtype::Number:
+            os << "Number";
+            break;
+        case Token::Subtype::Logical:
+            os << "Logical";
+            break;
+        case Token::Subtype::Error:
+            os << "Error";
+            break;
+        case Token::Subtype::Range:
+            os << "Range";
+            break;
+        case Token::Subtype::Math:
+            os << "Math";
+            break;
+        case Token::Subtype::Concatenation:
+            os << "Concatenation";
+            break;
+        case Token::Subtype::Intersection:
+            os << "Intersection";
+            break;
+        case Token::Subtype::Union:
+            os << "Union";
+            break;
+        default:
+            os << "Unknown";
+            break;
+    }
+}
+
+int main()
+{
+    std::vector<std::string>inputs {
+        "=1E+10+3+5",
+        "=3 * 4 + 5",
+        "=50",
+        "=1+1",
+        "=$A1",
+        "=$B$2",
+        "=SUM(B5:B15)",
+        "=SUM(B5:B15,D5:D15)",
+        "=SUM(B5:B15 A7:D7)",
+        "=SUM(sheet1!$A$1:$B$2)",
+        "=[data.xls]sheet1!$A$1",
+        "=SUM((A:A 1:1))",
+        "=SUM((A:A,1:1))",
+        "=SUM((A:A A1:B1))",
+        "=SUM(D9:D11,E9:E11,F9:F11)",
+        "=SUM((D9:D11,(E9:E11,F9:F11)))",
+        "=IF(P5=1.0,\"NA\",IF(P5=2.0,\"A\",IF(P5=3.0,\"B\",IF(P5=4.0,\"C\",IF(P5=5.0,\"D\",IF(P5=6.0,\"E\",IF(P5=7.0,\"F\",IF(P5=8.0,\"G\"))))))))",
+        "={SUM(B2:D2*B3:D3)}",
+        "=SUM(123 + SUM(456) + (45DATE(2002,1,6),0,IF(ISERROR(R[41]C[2]),0,IF(R13C3>=R[41]C[2],0, IF(AND(R[23]C[11]>=55,R[24]C[11]>=20),R53C3,0))))",
+        "=IF(R[39]C[11]>65,R[25]C[42],ROUND((R[11]C[11]*IF(OR(AND(R[39]C[11]>=55, "
+            "(R[40]C[11]>=20),AND(R[40]C[11]>=20,R11C3=\"YES\")),R[44]C[11],R[43]C[11]))+(R[14]C[11] "
+            "*IF(OR(AND(R[39]C[11]>=55,R[40]C[11]>=20),AND(R[40]C[11]>=20,R11C3=\"YES\")), "
+            "R[45]C[11],R[43]C[11])),0))"
+    };
+
+    for (const auto& input: inputs)
+    {
+        auto tokens = xlfparser::tokenize(input);
+        std::cout << input << " :" << std::endl;
+        for (const auto& token: tokens)
+            std::cout << '\t' << token.value(input) << '\t' << token.type() << '\t' << token.subtype() << std::endl;
+        std::cout << std::endl;
+    }
+
+    return 0;
+}
---
+++ tests/tests.cpp
@@ -0,0 +1,165 @@
+/*
+The MIT License
+
+Copyright (c) 2019 PyXLL Ltd. https://www.pyxll.com
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+*/
+#include "catch.hpp"
+#include "xlfparser.h"
+
+using namespace Catch::Matchers;
+
+
+TEST_CASE("Simple formula can be parsed correctly", "[xlfparser]")
+{
+    std::string formula("=1+2+3");
+    auto result = xlfparser::tokenize(formula);
+
+    REQUIRE(result.size() == 5);
+    CHECK_THAT(result[0].value(formula), Equals("1"));
+    CHECK(result[0].type() == xlfparser::Token::Type::Operand);
+    CHECK_THAT(result[1].value(formula), Equals("+"));
+    CHECK(result[1].type() == xlfparser::Token::Type::OperatorInfix);
+    CHECK_THAT(result[2].value(formula), Equals("2"));
+    CHECK(result[2].type() == xlfparser::Token::Type::Operand);
+    CHECK_THAT(result[3].value(formula), Equals("+"));
+    CHECK(result[3].type() == xlfparser::Token::Type::OperatorInfix);
+    CHECK_THAT(result[4].value(formula), Equals("3"));
+    CHECK(result[4].type() == xlfparser::Token::Type::Operand);
+}
+
+
+TEST_CASE("Wide formula can be parsed correctly", "[xlfparser]")
+{
+    std::wstring formula(L"=1+2+3");
+    auto result = xlfparser::tokenize(formula);
+
+    REQUIRE(result.size() == 5);
+
+    auto value0 = result[0].value(formula);
+    CHECK(std::wcscmp(value0.c_str(), L"1") == 0);
+    CHECK(result[0].type() == xlfparser::Token::Type::Operand);
+    CHECK(result[1].type() == xlfparser::Token::Type::OperatorInfix);
+
+    auto value2 = result[2].value(formula);
+    CHECK(std::wcscmp(value2.c_str(), L"2") == 0);
+    CHECK(result[2].type() == xlfparser::Token::Type::Operand);
+    CHECK(result[3].type() == xlfparser::Token::Type::OperatorInfix);
+
+    auto value4 = result[4].value(formula);
+    CHECK(std::wcscmp(value4.c_str(), L"3") == 0);
+    CHECK(result[4].type() == xlfparser::Token::Type::Operand);
+}
+
+
+TEST_CASE("Formula including a function parses correctly", "[xlfparser]")
+{
+    std::string formula("=SUM(1,2)");
+    auto result = xlfparser::tokenize(formula);
+
+    REQUIRE(result.size() == 5);
+    CHECK_THAT(result[0].value(formula), Equals("SUM"));
+    CHECK(result[0].type() == xlfparser::Token::Type::Function);
+    CHECK(result[0].subtype() == xlfparser::Token::Subtype::Start);
+
+    CHECK_THAT(result[1].value(formula), Equals("1"));
+    CHECK(result[1].type() == xlfparser::Token::Type::Operand);
+
+    CHECK_THAT(result[2].value(formula), Equals(","));
+    CHECK(result[2].type() == xlfparser::Token::Type::Argument);
+
+    CHECK_THAT(result[3].value(formula), Equals("2"));
+    CHECK(result[3].type() == xlfparser::Token::Type::Operand);
+
+    CHECK(result[4].type() == xlfparser::Token::Type::Function);
+    CHECK(result[4].subtype() == xlfparser::Token::Subtype::Stop);
+}
+
+
+TEST_CASE("Formula including nested functions parses correctly", "[xlfparser]")
+{
+    std::string formula("=outer(inner(1,2))");
+    auto result = xlfparser::tokenize(formula);
+
+    REQUIRE(result.size() == 7);
+
+    CHECK_THAT(result[0].value(formula), Equals("outer"));
+    CHECK(result[0].type() == xlfparser::Token::Type::Function);
+    CHECK(result[0].subtype() == xlfparser::Token::Subtype::Start);
+
+    CHECK_THAT(result[1].value(formula), Equals("inner"));
+    CHECK(result[1].type() == xlfparser::Token::Type::Function);
+    CHECK(result[1].subtype() == xlfparser::Token::Subtype::Start);
+
+    CHECK_THAT(result[2].value(formula), Equals("1"));
+    CHECK(result[2].type() == xlfparser::Token::Type::Operand);
+
+    CHECK_THAT(result[3].value(formula), Equals(","));
+    CHECK(result[3].type() == xlfparser::Token::Type::Argument);
+
+    CHECK_THAT(result[4].value(formula), Equals("2"));
+    CHECK(result[4].type() == xlfparser::Token::Type::Operand);
+
+    CHECK(result[5].type() == xlfparser::Token::Type::Function);
+    CHECK(result[5].subtype() == xlfparser::Token::Subtype::Stop);
+
+    CHECK(result[6].type() == xlfparser::Token::Type::Function);
+    CHECK(result[6].subtype() == xlfparser::Token::Subtype::Stop);
+}
+
+
+TEST_CASE("Scientific notation parses correctly", "[xlfparser]")
+{
+    std::string formula("=2.5E+10-3");
+    auto result = xlfparser::tokenize(formula);
+
+    REQUIRE(result.size() == 3);
+
+    CHECK_THAT(result[0].value(formula), Equals("2.5E+10"));
+    CHECK(result[0].type() == xlfparser::Token::Type::Operand);
+
+    CHECK_THAT(result[1].value(formula), Equals("-"));
+    CHECK(result[1].type() == xlfparser::Token::Type::OperatorInfix);
+
+    CHECK_THAT(result[2].value(formula), Equals("3"));
+    CHECK(result[2].type() == xlfparser::Token::Type::Operand);
+}
+
+
+TEST_CASE("Errors are parsed correctly", "[xlfparser]")
+{
+    std::vector<std::string> formulas{
+       "=#DIV/0!",
+       "=#VALUE!",
+       "=#REF!",
+       "=#NAME?",
+       "=#NUM!",
+       "=#N/A",
+       "=#SPILL!"
+    };
+
+    for (auto formula: formulas)
+    {
+        auto result = tokenize(formula);
+        REQUIRE(result.size() == 1);
+        CHECK(result[0].type() == Token::Type::Operand);
+        CHECK(result[0].subtype() == Token::Subtype::Error);
+    }
+}
